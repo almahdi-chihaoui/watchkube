d483f34578b769c37f0549f6026e17685ef033a7
almahdi-chihaoui <almahdichihaoui@gmail.com>
2019-01-04 21:52:36 +0100

    Documented the script and added support for namespaces

--- src/kubernetes/unix/applyChanges.sh
+++ src/kubernetes/unix/applyChanges.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+# $1: local path
+# $2: selector
+# $3: remote path
+# $4: container name
+# $5: namespace
+
+# Get the pod's name from the selector $2
 POD_NAME="$(kubectl describe po -l $2 | grep ^Name: | head -1 | awk '{print $2}')"
-kubectl cp $1 $POD_NAME:$3 $4 $5 || exit 1
+
+# We test if container name ($4) and namespace ($5) were given, and we execute the kubectl with otions -n
+# and -c accordingly
+if [-z $4] && [-z $5]; then
+  kubectl cp $1 $POD_NAME:$3 || exit 1
+elif [-n $4] && [-z $5]; then
+  kubectl cp $1 $POD_NAME:$3 -c $4 || exit 1
+elif [-z $4] && [-n $5]; then
+  kubectl cp $1 $POD_NAME:$3 -n $5 || exit 1
+elif [-n $4] && [-n $5]; then
+  kubectl cp $1 $POD_NAME:$3 -c $4 -n $5 || exit 1
+fi
