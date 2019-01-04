44fe298b9e9d78c7e94dc4e4d18e32b43e9d8b1e
almahdi-chihaoui <almahdichihaoui@gmail.com>
2019-01-04 21:53:43 +0100

    Documented the delete script, added support for the namepace and remove folder option

--- src/kubernetes/unix/deleteFile.sh
+++ src/kubernetes/unix/deleteFile.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-POD_NAME="$(kubectl describe po -l $1 | grep ^Name: | head -1 | awk '{print $2}')"
-kubectl exec $POD_NAME -it $3 $4 -- rm $2 || exit 1
+# $1: remove folder option
+# $2: selector
+# $3: remote path
+# $4: container name
+# $5: namespace
+
+# Get the pod's name from the selector $1
+POD_NAME="$(kubectl describe po -l $2 | grep ^Name: | head -1 | awk '{print $2}')"
+
+# We test if container name ($4) and namespace ($5) were given, and we execute the kubectl with otions -n
+# and -c accordingly
+if [-z $4] && [-z $5]; then
+  kubectl exec $POD_NAME -it -- rm $1 $3 || exit 1
+elif [-n $4] && [-z $5]; then
+  kubectl exec $POD_NAME -it -c $4 -- rm $1 $3 || exit 1
+elif [-z $4] && [-n $5]; then
+  kubectl exec $POD_NAME -it -n $5 -- rm $1 $3 || exit 1
+elif [-n $4] && [-n $5]; then
+  kubectl exec $POD_NAME -it -c $4 -n $5 -- rm $1 $3 || exit 1
+fi
