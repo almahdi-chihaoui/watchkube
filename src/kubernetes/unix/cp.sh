62b39b6bcf9c0f57db0bf6c1d8f8fa1330d27794
almahdi-chihaoui <almahdichihaoui@gmail.com>
2019-01-14 01:12:42 +0100

    addd the reload cmd to cp unix script

--- src/kubernetes/unix/cp.sh
+++ src/kubernetes/unix/cp.sh
@@ -5,6 +5,7 @@
 # $3: remote path
 # $4: container name
 # $5: namespace
+# $6: reload
 
 # Get the pod's name from the selector $2 and the namespace $5
 if [ -z $5 ]; then
@@ -23,4 +24,17 @@ elif [ -z $4 ] && [ -n $5 ]; then
   kubectl cp $1 $POD_NAME:$3 -n $5 || exit 1
 elif [ -n $4 ] && [ -n $5 ]; then
   kubectl cp $1 $POD_NAME:$3 -c $4 -n $5 || exit 1
+fi
+
+# Reload the container if reload ($6) is equal to -r
+if [ $6='-r' ]; then
+  if [ -z $4 ] && [ -z $5 ]; then
+    kubectl exec -it $POD_NAME -- /bin/sh -c "kill 1" || exit 1
+  elif [ -n $4 ] && [ -z $5 ]; then
+    kubectl exec -it $POD_NAME -c $4 -- /bin/sh -c "kill 1" || exit 1
+  elif [ -z $4 ] && [ -n $5 ]; then
+    kubectl exec -it $POD_NAME -n $5 -- /bin/sh -c "kill 1" || exit 1
+  elif [ -n $4 ] && [ -n $5 ]; then
+    kubectl exec -it $POD_NAME -c $4 -n $5 -- /bin/sh -c "kill 1" || exit 1
+  fi
 fi
