f268889b0aebf1e19a53ffa54b6de99566eaa230
almahdi-chihaoui <almahdichihaoui@gmail.com>
2019-01-14 01:23:21 +0100

    added reload to cmds

--- src/kubernetes/executeCmds.js
+++ src/kubernetes/executeCmds.js
@@ -14,28 +14,28 @@ const execScriptFileCmd = (os, scriptFile) => {
   return cmd[os];
 };
 
-const applyChanges = (platform, os, selector, localPath, remotePath, containerName, nameSpace) => {
+const applyChanges = (platform, os, selector, localPath, remotePath, containerName, nameSpace, reload) => {
   const scriptFile = {
     unix: 'cp.sh',
     windows: 'cp',
   }
-  return `${path.join(MAIN_DIR, `/src/${platform}/${os}/${scriptFile[os]}`)} ${localPath} ${selector} ${remotePath} ${containerName} ${nameSpace}`;
+  return `${path.join(MAIN_DIR, `/src/${platform}/${os}/${scriptFile[os]}`)} ${localPath} ${selector} ${remotePath} ${containerName} ${nameSpace} ${reload}`;
 };
 
-const deleteFile = (platform, os, selector, remotePath, containerName, nameSpace) => {
+const deleteFile = (platform, os, selector, remotePath, containerName, nameSpace, reload) => {
   const scriptFile = {
     unix: 'rm.sh file',
     windows: 'rm file',
   }
-  return `${path.join(MAIN_DIR, `/src/${platform}/${os}/${scriptFile[os]}`)} ${selector} ${remotePath} ${containerName} ${nameSpace}`;
+  return `${path.join(MAIN_DIR, `/src/${platform}/${os}/${scriptFile[os]}`)} ${selector} ${remotePath} ${containerName} ${nameSpace} ${reload}`;
 };
 
-const deleteFolder = (platform, os, selector, remotePath, containerName, nameSpace) => {
+const deleteFolder = (platform, os, selector, remotePath, containerName, nameSpace, reload) => {
   const scriptFile = {
     unix: 'rm.sh fldr',
     windows: 'rm fldr',
   }
-  return `${path.join(MAIN_DIR, `/src/${platform}/${os}/${scriptFile[os]}`)} ${selector} ${remotePath} ${containerName} ${nameSpace}`;
+  return `${path.join(MAIN_DIR, `/src/${platform}/${os}/${scriptFile[os]}`)} ${selector} ${remotePath} ${containerName} ${nameSpace} ${reload}`;
 };
 
 module.exports = {
