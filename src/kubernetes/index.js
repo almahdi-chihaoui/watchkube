f55f9639a76f243b06d578f75a06bbcae8045c95
almahdi-chihaoui <almahdichihaoui@gmail.com>
2019-01-20 21:40:51 +0100

    Format and issues fix

--- src/kubernetes/index.js
+++ src/kubernetes/index.js
@@ -9,14 +9,68 @@ const {
   deleteFolder,
 } = require('./executeCmds');
 
-const updateKube = (os, event, selector, localPath, remotePath, containerName, nameSpace, reload) => {
+const updateKube = (
+  os,
+  event,
+  selector,
+  localPath,
+  remotePath,
+  containerName,
+  nameSpace,
+) => {
   const platform = 'kubernetes';
   const mapEventsToCmds = {
-    change: () => execScriptFileCmd(os, applyChanges(platform, os, selector, localPath, remotePath, containerName, nameSpace, reload)),
-    add: () => execScriptFileCmd(os, applyChanges(platform, os, selector, localPath, remotePath, containerName, nameSpace, reload)),
-    unlink: () => execScriptFileCmd(os, deleteFile(platform, os, selector, remotePath, containerName, nameSpace, reload)),
-    addDir: () => execScriptFileCmd(os, applyChanges(platform, os, selector, localPath, remotePath, containerName, nameSpace, reload)),
-    unlinkDir: () => execScriptFileCmd(os, deleteFolder(platform, os, selector, remotePath, containerName, nameSpace, reload)),
+    change: () => execScriptFileCmd(
+      os,
+      applyChanges(
+        platform,
+        os,
+        selector,
+        localPath,
+        remotePath,
+        containerName,
+        nameSpace,
+      )),
+    add: () => execScriptFileCmd(
+      os,
+      applyChanges(
+        platform,
+        os,
+        selector,
+        ocalPath,
+        remotePath,
+        containerName,
+        nameSpace,
+      )),
+    unlink: () => execScriptFileCmd(
+      os,
+      deleteFile(
+        platform,
+        os,
+        selector,
+        remotePath,
+        containerName,
+        nameSpace,
+      )),
+    addDir: () => execScriptFileCmd(
+      os,
+      applyChanges(platform,
+        os, selector,
+        ocalPath,
+        remotePath,
+        containerName,
+        nameSpace,
+      )),
+    unlinkDir: () => execScriptFileCmd(
+      os,
+      deleteFolder(
+        platform,
+        os,
+        selector,
+        remotePath,
+        containerName,
+        nameSpace,
+      )),
   };
   return exec(mapEventsToCmds[event]());
 }
